f7d51cd79f41b31ffe9f201de7681e1efa021aea
rloqvist <rasmus@catfarm.se>
2023-04-09 16:48:53 +0200

    Change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url)
-  }),
+  adapter: netlify(),
   integrations: [react()],
 });
