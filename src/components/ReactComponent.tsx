bafbe93f9b275cfa5ba2214e371bd2addb300a39
rloqvist <rasmus@catfarm.se>
2023-04-09 16:17:05 +0200

    Boilerplate

---
+++ src/components/ReactComponent.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import {prop} from "styled-tools"
+
+export const StyledReactComponent = div`
+  color: ${prop("color")};
+`
+
+const ReactComponent = ({someProp}) => {
+  return (
+    <StyledReactComponent color="purple">
+      Your react prop is: {someProp}
+    </StyledReactComponent>
+  )
+}
